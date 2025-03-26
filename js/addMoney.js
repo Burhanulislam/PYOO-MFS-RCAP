af94d50d2fb2d45983a0df4a222219528a761936
Burhanul <burhanulwdd@gmail.com>
2025-03-27 01:20:43 +0600

    Practiced Add Money Function

--- js/addMoney.js
+++ js/addMoney.js
@@ -3,9 +3,23 @@ document.getElementById('add-money-btn')
         event.preventDefault();
         
         const addMoney = document.getElementById('input-add-money').value;
+        const addMoneyNumber = parseFloat(addMoney);
 
         const pinNumber = document.getElementById('input-pin-number').value;
 
         console.log(addMoney, pinNumber);
 
+        // wrong way to verify pin number
+        if(pinNumber === '1234'){
+            const balance = document.getElementById('account-balance').innerText;
+            const balanceNumber = parseFloat(balance);
+            console.log('account balance added');
+            
+            const newBalance = balanceNumber + addMoneyNumber;
+
+            document.getElementById('account-balance').innerText = newBalance;
+        }
+        else{
+            alert('Failed to add money.')
+        }
     });
